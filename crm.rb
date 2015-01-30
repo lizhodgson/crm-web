d0778230f4595b166ee0b17358982cab336a081e
lizhodgson <lizhodgson@gmail.com>
2015-01-30 13:49:02 -0500

    trying to make CRM look pretty

--- crm.rb
+++ crm.rb
@@ -1,12 +1,17 @@
-	require './contact'
-	require './rolodex'
-	require 'sinatra'
+require 'sinatra'
+require "sinatra/reloader"
+require './contact'
+require	'./rolodex'
 
-	$rolodex = Rolodex.new
+
+$rolodex = Rolodex.new
+contact = $rolodex.find(1000)
+	# $rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
 
 get '/' do
-	@crm_name = "My CRM"
-	erb :index
+	@crm_name = "Liz's CRM"
+  erb :index
 end
 
 get '/contacts' do
@@ -18,8 +23,50 @@ get '/contacts/new' do
 	erb :new
 end
 
+get "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  	if @contact
+  		erb :show_contact
+  	else
+  		raise Sinatra::NotFound
+		end
+end
+
+get "/contacts/:id/edit" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 post '/contacts' do
 	contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	$rolodex.add_contact(contact)
 	redirect to('/contacts')
+end
+
+put "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+    
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    $rolodex.remove_contact(@contact)
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
 end
--- rolodex.rb
+++ rolodex.rb
@@ -4,11 +4,47 @@ class Rolodex
 	def initialize
 		@contacts = []
 		@id = 1000
+		
 	end
-
+	
 	def add_contact(contact)
 		contact.id = @id
 		@contacts << contact
 		@id += 1
 	end
+
+
+	def modify_a_contact(contact)
+
+		puts "\nChoose an attribute to modify: \n[1] First Name \n[2] Last Name \n[3] Email \n[4] Note"
+		answer = gets.chomp.to_i
+
+		case answer
+		when 1 
+			puts "Enter a modified First Name"
+			firstname = gets.chomp.capitalize
+			contact.first_name = firstname
+		when 2
+			puts "Enter a modified Last Name"
+			lastname = gets.chomp.capitalize
+			contact.last_name = lastname
+		when 3 
+			puts "Enter a modified Email Address"
+			emailaddress = gets.chomp
+			contact.email = emailaddress
+		when 4 
+			puts "Enter a modified note"
+			newnote = gets.chomp
+			contact.note = newnote
+		end
+		
+	end
+
+	def find(contact_id)
+    @contacts.find {|contact| contact.id == contact_id }
+  end
+
+  def remove_contact(contact)
+    @contacts.delete(contact)
+  end
 end
